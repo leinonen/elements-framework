c3d3270e5dbd390c2c3e67302b19f1a386d5d776
Peter Leinonen <peter@leinonen.se>
2016-10-23 18:12:01 +0200

    fix header

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import E from '../lib/elements';
 
 const Header = E.h1()
   .css('bold beautiful')
-  .text('elements')
   .on('click', function(/*e*/) {
     this.toggleClass('beautiful');
   })
   .children([
+    E.span().text('elements'),
     E.span()
       .css('micro')
       .text('micro DOM framework')
